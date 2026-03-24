0feb320e11c7a3e79864148c8854b63d20740ded
Chaitan Divagi <natiahc@gmail.com>
2026-03-24 21:10:51 +0530

    Update main.py

--- control_server/main.py
+++ control_server/main.py
@@ -3,10 +3,12 @@ from database import Base, engine
 from routes_auth import router as auth_router
 from routes_nodes import router as nodes_router
 from routes_files import router as files_router
+from routes_policy import router as policy_router
 
 app.include_router(auth_router)
 app.include_router(nodes_router)
 app.include_router(files_router)
+app.include_router(policy_router)
 
 app = FastAPI(
     title="Secure Decentralized Storage Control Server",
